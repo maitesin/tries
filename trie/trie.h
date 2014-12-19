6bdc14cadc540d0990bfcaaf656928436093e6ae
Maitesin <violador.de.segmentos@gmail.com>
2014-12-19 09:58:35 +0000

    Some documentation

--- trie/trie.h
+++ trie/trie.h
@@ -7,6 +7,9 @@
 #include <array>
 #include <memory>
 
+/*************************
+ *** Node for the Trie ***
+ *************************/
 template <class T, size_t R>
 struct node {
   T value;
@@ -24,6 +27,9 @@ struct node {
   }
 };
 
+/************
+ *** Trie ***
+ ************/
 template <class T, size_t R>
 class trie {
  public:
@@ -37,13 +43,57 @@ class trie {
     clean(std::move(root));
   }
   // Methods
+  /*
+   * This method return the value stored in the trie
+   * for a specific key provided as parameter. If there
+   * is no the key in the trie return default value for
+   * the value type.
+   */
   const T & get(const std::string key);
+
+  /*
+   * This method is used to add a new key to the trie.
+   * You must provide the key and the value to store.
+   * If the key was not before in the trie it will
+   * create the path to it and store the value.
+   * Otherwise, if the key was already in the trie it
+   * will update the value of the key with the new one.
+   */
   void put(const std::string & key,
            const T & value);
+
+  /*
+   * This method return the amount of elements stored
+   * in the trie.
+   */
   int size() {return s;}
+
+  /*
+   * This is an auxiliary method to print in the screen
+   * the current set of keys of the trie.
+   */
   void show();
+
+  /*
+   * This method is to remove a value from the trie.
+   * You must provide the key that holds the value.
+   * If the key exist deletes the value and delete
+   * the path to it if there are no other keys that
+   * are using it. Otherwise, if the key is not there
+   * nothing will happen.
+   */
   void remove(const std::string & key);
+
+  /*
+   * This method tells you if a given key is in the
+   * trie.
+   */
   bool contains(const std::string & key);
+
+  /*
+   *
+   *
+   */
   std::vector<std::string> get_keys_with_prefix(const std::string & prefix);
   std::vector<std::string> get_keys();
 
