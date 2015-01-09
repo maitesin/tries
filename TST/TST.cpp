950b40b38992d9151b9aba2b0cad30fb3cffd9fd
Maitesin <violador.de.segmentos@gmail.com>
2015-01-09 17:50:20 +0000

    Implemented all the methods in TST but remove (I have to think about it).
    Added the a Makefile and a test file. I have to take care of the compile errors

--- TST/TST.cpp
+++ TST/TST.cpp
@@ -5,7 +5,7 @@
 #include "./TST.h"
 
 template <class T>
-const T & TST::TST::get(const std::string & key) {
+const T & TST::TST<T>::get(const std::string & key) {
 	node_ptr<T> node (get(root, key, 0));
 	if (node != nullptr) {
 		aux_ret = node->value;
@@ -18,9 +18,9 @@ const T & TST::TST::get(const std::string & key) {
 }
 
 template <class T>
-node_ptr<T> TST::TST::get(TST::node_ptr<T> & n,
-			  const std::string & key,
-			  unsigned int d) {
+TST::node_ptr<T> TST::TST<T>::get(TST::node_ptr<T> & n,
+				  const std::string & key,
+				  unsigned int d) {
 	if (key.size() == d) {
 		if (n == nullptr)
 			return nullptr;
@@ -38,14 +38,14 @@ node_ptr<T> TST::TST::get(TST::node_ptr<T> & n,
 }
 
 template <class T>
-void TST::TST::put(const std::string & key,
+void TST::TST<T>::put(const std::string & key,
 		   const T & value) {
 	root = put(std::move(root), key, value, 0);
 	++s;
 }
 
 template <class T>
-node_ptr<T> TST::TST::put(TST::node_ptr<T> n,
+TST::node_ptr<T> TST::TST<T>::put(TST::node_ptr<T> n,
 			  const std::string & key,
 			  unsigned int d) {
 	if (key.size() == d) {
@@ -72,7 +72,7 @@ node_ptr<T> TST::TST::put(TST::node_ptr<T> n,
 }
 
 template <class T>
-void TST::TST::clean(TST::node_ptr<T> n) {
+void TST::TST<T>::clean(TST::node_ptr<T> n) {
 	if (n->left   == nullptr) clean(n->left);
 	if (n->middle == nullptr) clean(n->middle);
 	if (n->right  == nullptr) clean(n->right);
@@ -80,12 +80,12 @@ void TST::TST::clean(TST::node_ptr<T> n) {
 }
 
 template <class T>
-bool TST::TST::contains(const std::string & key) {
+bool TST::TST<T>::contains(const std::string & key) {
 	return contains(root, key, 0);
 }
 
 template <class T>
-bool TST::TST::contains(TST::node_ptr<T> & n,
+bool TST::TST<T>::contains(TST::node_ptr<T> & n,
 			const std::string & key,
 			unsigned int d) {
 	if (key.size() == d) {
@@ -111,44 +111,68 @@ bool TST::TST::contains(TST::node_ptr<T> & n,
 }
 
 template <class T>
-void TST::TST::remove(const std::string & key) {
-
+void TST::TST<T>::remove(const std::string & key) {
+	//think about it
 }
 
 template <class T>
-bool TST::TST::remove(TST::node_ptr<T> & n,
+bool TST::TST<T>::remove(TST::node_ptr<T> & n,
 		      const std::string & key,
 		      unsigned int d) {
-
+	//think about it
 }
 
 template <class T>
-std::vector<std::string> TST::TST::get_keys() {
-
+std::vector<std::string> TST::TST<T>::get_keys() {
+	vec_ptr vec;
+	vec = vec_ptr(new std::vector<std::string>());
+	get_keys_with_prefix(root, "", 0, vec);
+	return *vec;
 }
 
 template <class T>
-std::vector<std::string> TST::TST::get_keys_with_prefix(const std::string & prefix) {
-
+std::vector<std::string> TST::TST<T>::get_keys_with_prefix(const std::string & prefix) {
+	vec_ptr vec;
+	vec = vec_ptr(new std::vector<std::string>());
+	get_keys_with_prefix(root, prefix, 0, vec);
+	return *vec;
 }
 
 template <class T>
-void TST::TST::get_keys_with_prefix(TST::node_ptr<T> & n,
+void TST::TST<T>::get_keys_with_prefix(TST::node_ptr<T> & n,
 				    std::string prefix,
 				    unsigned int d,
 				    TST::vec_ptr & v) {
-
+	if (prefix.size() == d) {
+		gather_keys(n, prefix, v);
+	} else {
+		if (n->c < prefix[d] && n->left != nullptr)
+			get_keys_with_prefix(n->left, prefix, d, v);
+		if (n->c == prefix[d] && n->middle != nullptr)
+			get_keys_with_prefix(n->middle, prefix, d+1, v);
+		if (n->c > prefix[d] && n->right != nullptr)
+			get_keys_with_prefix(n->right, prefix, d, v);
+	}
 }
 
 
 template <class T>
-void TST::TST::gather_keys(TST::node_ptr<T> & n,
+void TST::TST<T>::gather_keys(TST::node_ptr<T> & n,
 			   std::string prefix,
 			   TST::vec_ptr & v) {
-
+	if (n == nullptr) return;
+	if (n->value != def) {
+		v->push_back(prefix);
+	}
+	gather_keys(n->left, prefix, v);
+	gather_keys(n->middle, prefix + n->c, v);
+	gather_keys(n->right, prefix, v);
 }
 
 template <class T>
-void TST::TST::show() {
-
+void TST::TST<T>::show() {
+	std::cout << "new TST to show" << std::endl;
+	for (auto key : get_keys()) {
+		std::cout << key << std::endl;
+	}
 }
---
+++ TST/test.cpp
@@ -0,0 +1,54 @@
+#include <string>
+#include <iostream>
+#include "TST.cpp"
+
+int main(void) {
+
+	TST::TST<int> t;
+	std::string hello = "Hello", world = "World", wololo = "Wololo", he = "He", kthulu = "Kthulu", no = "No", hes = "Hes";
+	
+	t.put(hello, 1);  
+	t.put(world, 2);
+	t.put(wololo, 3);
+	t.show();
+	std::cout << "Current size should be 3; size = " << t.size() << std::endl;
+	
+	std::cout << "Get Hello = " << t.get(hello) << std::endl;
+	std::cout << "Get World = " << t.get(world) << std::endl;
+	std::cout << "Get Wololo = " << t.get(wololo) << std::endl;
+	std::cout << "Get Kthulu = " << t.get(kthulu) << std::endl;
+	t.show();
+	t.put(kthulu, 4);
+	t.show();
+	std::cout << "Get Kthulu = " << t.get(kthulu) << std::endl;
+	std::cout << "Get He = " << t.get(he) << std::endl;
+	t.put(he, 5);
+	std::cout << "Get He = " << t.get(he) << std::endl;
+	
+	t.show();
+	
+	std::cout << "Contains He? " << t.contains(he) << std::endl;
+	std::cout << "Contains No? " << t.contains(no) << std::endl;
+	std::cout << "Contains Hes? " << t.contains(hes) << std::endl;
+	
+	std::cout << "Keys with prefix He" << std::endl;
+	for (auto key : t.get_keys_with_prefix("He")) {
+		std::cout << key << std::endl;
+	}
+	
+	std::cout << "Keys with prefix Hi" << std::endl;
+	for (auto key : t.get_keys_with_prefix("Hi")) {
+		std::cout << key << std::endl;
+	}
+	
+	std::cout << "Removing Hello" << std::endl;
+	t.remove(hello);
+	t.show();
+	std::cout << "Removing He" << std::endl;
+	t.remove(he);
+	t.show();
+	std::cout << "Removing no" << std::endl;
+	t.remove(no);
+	t.show();
+	return 0;
+}
