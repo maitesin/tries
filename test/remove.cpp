02d78119cbe75f7480c8651f5ca6c0fa22e500b6
Maitesin <violador.de.segmentos@gmail.com>
2015-02-10 19:43:12 +0000

    Added the file remove.cpp to test the performance of the remove operation. Moreover, added few lines in the Makefile to be able to build

---
+++ test/remove.cpp
@@ -0,0 +1,109 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include <vector>
+
+// Conditional include for dependencies
+#ifdef TRIE
+#include <trie.cpp>
+#endif
+#ifdef TERNARY
+#include <TST.cpp>
+#endif
+#ifdef RADIX
+#include <radix_tree.cpp>
+#endif
+#ifdef MAP
+#include <map>
+#endif
+#ifdef UMAP
+#include <unordered_map>
+#endif
+
+// Definition of the default parameters
+#ifndef MIN_SIZE
+#define MIN_SIZE 10
+#endif
+#ifndef MAX_SIZE
+#define MAX_SIZE 100000000
+#endif
+#ifndef SECONDS_LOOP
+#define SECONDS_LOOP 10.0
+#endif
+#ifndef SALT
+#define SALT 0
+#endif
+#ifdef ALPHANUM
+#define ALPHABET "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+#define LENGTH 62
+#else
+#define LENGTH 256
+#endif
+
+std::string get_random_string(unsigned int len) {
+	std::string result = "";
+	for (unsigned int i = 0; i < len; ++i){
+		#ifdef ALPHANUM
+		result += ALPHABET[rand()%LENGTH];
+		#else
+		result += static_cast<char>(rand()%LENGTH);
+		#endif
+	}
+	return result;
+}
+
+int main(void) {
+	time_t t_init;
+	float us;
+	int size, counter;
+	
+	srand(SALT);
+
+	for (size = MIN_SIZE; size <= MAX_SIZE; size *= 2) {
+#ifdef TRIE
+		Trie::trie<int, 256> t;
+#endif
+#ifdef TERNARY
+		TST::tst<int> t;
+#endif
+#ifdef RADIX
+		RadixTree::radix_tree<int, 256> t;
+#endif
+#ifdef MAP
+		std::map<std::string, int> m;
+#endif
+#ifdef UMAP
+#define MAP
+		std::unordered_map<std::string, int> m;
+#endif
+		std::vector<std::string> elems;
+		std::string aux;
+		counter = 0;
+
+		for (unsigned int i = 0; i < 5000000; ++i) {
+			aux = get_random_string(rand()%size);
+			if (i%1000 == 0)
+				elems.push_back(aux);
+#ifndef MAP
+			t.put(aux, 1);
+#else
+			m.insert(std::pair<std::string, int>(aux, 1));
+#endif
+		}
+
+		
+		t_init = clock();
+		while (clock() - t_init < SECONDS_LOOP * CLOCKS_PER_SEC && counter < elems.size()) {
+#ifndef MAP
+			t.remove(elems[counter]);
+#else
+			m.erase(elems[counter]);
+#endif
+			++counter;			
+		}
+		us = 1e6*float(clock() - t_init)/CLOCKS_PER_SEC;
+		std::cout << size << "\t" << us/counter << std::endl;
+	}
+
+	return 0;
+}
