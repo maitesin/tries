7d50366f097c10df2064b6dd7a65562971e622db
Maitesin <violador.de.segmentos@gmail.com>
2015-02-11 11:24:38 +0000

    Added 3 new python scripts to allow me to create the plots

---
+++ test/plots/remove_not_found.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python2.7
+
+from numpy import *
+from pylab import *
+from matplotlib import rc, rcParams
+
+rc('font',**{'family':'serif','serif':['Computer Modern']})
+
+trie = genfromtxt('../trie_remove_not_found.output')
+tst = genfromtxt('../tst_remove_not_found.output')
+radix = genfromtxt('../radix_remove_not_found.output')
+_map = genfromtxt('../map_remove_not_found.output')
+umap  = genfromtxt('../umap_remove_not_found.output')
+
+######## TIME ########
+plot(trie[:,0], trie[:,1], '-o', label='Trie')
+hold(True)
+plot(tst[:,0], tst[:,1], '-o', label='Ternary Search Tree')
+plot(radix[:,0], radix[:,1], '-o', label='Radix Tree')
+plot(_map[:,0], _map[:,1], '-o', label='STL ordered Map')
+plot(umap[:,0], umap[:,1], '-o', label='STL unordered Map')
+
+xlabel('Max length of the string')
+ylabel('Time(ms)')
+title('Remove test (not found)')
+legend(loc='best')
+grid(True)
+savefig('remove_not_found_time_ALL.eps')
+
+hold(False)
+plot(tst[:,0], tst[:,1], '-o', label='Ternary Search Tree')
+hold(True)
+plot(radix[:,0], radix[:,1], '-o', label='Radix Tree')
+plot(_map[:,0], _map[:,1], '-o', label='STL ordered Map')
+plot(umap[:,0], umap[:,1], '-o', label='STL unordered Map')
+
+xlabel('Max length of the string')
+ylabel('Time(ms)')
+title('Remove test (not found)')
+legend(loc='best')
+grid(True)
+savefig('remove_not_found_time_TRMU.eps')
+
+hold(False)
+plot(radix[:,0], radix[:,1], '-o', label='Radix Tree')
+hold(True)
+plot(_map[:,0], _map[:,1], '-o', label='STL ordered Map')
+plot(umap[:,0], umap[:,1], '-o', label='STL unordered Map')
+
+xlabel('Max length of the string')
+ylabel('Time(ms)')
+title('Remove test (not found)')
+legend(loc='best')
+grid(True)
+savefig('remove_not_found_time_RMU.eps')
