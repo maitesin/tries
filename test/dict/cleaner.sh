52ca36a8517a05628f4cf99b65125fa139337807
Oscar Forner Martinez <oscar_martinez@programmingresearch.com>
2015-11-30 09:22:09 +0000

    Changes to clean the file

--- test/dict/cleaner.sh
+++ test/dict/cleaner.sh
@@ -3,31 +3,4 @@
 SOURCE=$1
 TARGET=$2
 
-cat ${SOURCE} \
-    | grep -v "á" \
-    | grep -v "à" \
-    | grep -v "é" \
-    | grep -v "è" \
-    | grep -v "í" \
-    | grep -v "ú" \
-    | grep -v "ó" \
-    | grep -v "ò" \
-    | grep -v "ถุ" \
-    | grep -v "ภ" \
-    | grep -v "●" \
-    | grep -v "ñ" \
-    | grep -v "£" \
-    | grep -v "ำั" \
-    | grep -v "ก" \
-    | grep -v "ๅ" \
-    | grep -v "นี" \
-    | grep -v "Â" \
-    | grep -v "â" \
-    | grep -v "Ã" \
-    | grep -v "¶" \
-    | grep -v "±" \
-    | grep -v "ö" \
-    | grep -v "´" \
-    | grep -v "ﾅ" \
-    | grep -v "�" \
-    > ${TARGET}
+tr -dc [:graph:][:cntrl:] < ${SOURCE} > ${TARGET}
