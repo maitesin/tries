956a90083f8138ed0d5d6c8efc3031d56e48cdce
Maitesin <violador.de.segmentos@gmail.com>
2016-01-21 21:18:12 +0000

    Formated the trie with clang-format utility.

--- trie/lib/trie.cpp
+++ trie/lib/trie.cpp
@@ -5,242 +5,236 @@
 #include "./trie.h"
 
 template <class T, size_t R>
-const T & Trie::trie<T,R>::find(const std::string & key) {
-	if (key != "") {
-		node_ptr node (find(root, key, 0));
-		if (node != nullptr){
-			aux_ret = node->value;
-			node.release();
-			return aux_ret;
-		} else {
-			return def;
-		}
-	}
-	return def;
+const T &Trie::trie<T, R>::find(const std::string &key) {
+  if (key != "") {
+    node_ptr node(find(root, key, 0));
+    if (node != nullptr) {
+      aux_ret = node->value;
+      node.release();
+      return aux_ret;
+    } else {
+      return def;
+    }
+  }
+  return def;
 }
 
 template <class T, size_t R>
-typename Trie::trie<T,R>::node_ptr Trie::trie<T,R>::find(Trie::trie<T,R>::node_ptr & n,
-					 const std::string & key,
-					 size_t d) {
-	if (key.size() == d) {
-		if (n == nullptr) {
-			return nullptr;
-		} else {
-			if (n->value != def)
-				return node_ptr(n.get());
-			else
-				return nullptr;
-		}
-	} else {
-		if (n->sons[key[d]] != nullptr) {
-			return find(n->sons[key[d]], key, d+1);
-		}
-	}
-	return nullptr;
+typename Trie::trie<T, R>::node_ptr
+Trie::trie<T, R>::find(Trie::trie<T, R>::node_ptr &n, const std::string &key,
+                       size_t d) {
+  if (key.size() == d) {
+    if (n == nullptr) {
+      return nullptr;
+    } else {
+      if (n->value != def)
+        return node_ptr(n.get());
+      else
+        return nullptr;
+    }
+  } else {
+    if (n->sons[key[d]] != nullptr) {
+      return find(n->sons[key[d]], key, d + 1);
+    }
+  }
+  return nullptr;
 }
 
 template <class T, size_t R>
-void Trie::trie<T,R>::insert(const std::string & key,
-			  const T & value) {
-	if (key != ""){
-		bool created = false;
-		root = insert(std::move(root), key, value, 0, created);
-		if (created)
-			++s;
-	}
+void Trie::trie<T, R>::insert(const std::string &key, const T &value) {
+  if (key != "") {
+    bool created = false;
+    root = insert(std::move(root), key, value, 0, created);
+    if (created)
+      ++s;
+  }
 }
 
 template <class T, size_t R>
-typename Trie::trie<T,R>::node_ptr Trie::trie<T,R>::insert(Trie::trie<T,R>::node_ptr n,
-					 const std::string & key,
-					 const T & value,
-					 size_t d,
-					 bool & created) {
-	if (key.size() == d) {
-		if (n->value == def)
-			created = true;
-		n->value = value;
-		return n;
-	} else {
-		if (n->sons[key[d]] == nullptr) {
-			n->sons[key[d]] = node_ptr(new node());
-			++n->s;
-		}
-		n->sons[key[d]] = insert(std::move(n->sons[key[d]]), key, value, d+1, created);
-		return n;
-	}
+typename Trie::trie<T, R>::node_ptr
+Trie::trie<T, R>::insert(Trie::trie<T, R>::node_ptr n, const std::string &key,
+                         const T &value, size_t d, bool &created) {
+  if (key.size() == d) {
+    if (n->value == def)
+      created = true;
+    n->value = value;
+    return n;
+  } else {
+    if (n->sons[key[d]] == nullptr) {
+      n->sons[key[d]] = node_ptr(new node());
+      ++n->s;
+    }
+    n->sons[key[d]] =
+        insert(std::move(n->sons[key[d]]), key, value, d + 1, created);
+    return n;
+  }
 }
 
 template <class T, size_t R>
-void Trie::trie<T,R>::clear(Trie::trie<T,R>::node_ptr n) {
-	for (size_t i = 0; i < n->r; ++i) {
-		if (n->sons[i] != nullptr) clear(std::move(n->sons[i]));
-	}
-	n.reset();
+void Trie::trie<T, R>::clear(Trie::trie<T, R>::node_ptr n) {
+  for (size_t i = 0; i < n->r; ++i) {
+    if (n->sons[i] != nullptr)
+      clear(std::move(n->sons[i]));
+  }
+  n.reset();
 }
 
 template <class T, size_t R>
-bool Trie::trie<T,R>::contains(const std::string & key) {
-	return contains(root, key, 0);
+bool Trie::trie<T, R>::contains(const std::string &key) {
+  return contains(root, key, 0);
 }
 
 template <class T, size_t R>
-bool Trie::trie<T,R>::contains(Trie::trie<T,R>::node_ptr & n,
-			       const std::string & key,
-			       size_t d) {
-	if (key.size() ==d) {
-		if (n == nullptr) return false;
-		return n->value != def;
-	} else {
-		if (n->sons[key[d]] != nullptr) {
-			return contains(n->sons[key[d]], key, d+1);
-		}
-		return false;
-	}
-}
-
-
-template <class T, size_t R>
-void Trie::trie<T,R>::erase(const std::string & key) {
-	bool decrease = false;
-	erase(root, key, 0, decrease);
-	if (decrease)
-		--s;
+bool Trie::trie<T, R>::contains(Trie::trie<T, R>::node_ptr &n,
+                                const std::string &key, size_t d) {
+  if (key.size() == d) {
+    if (n == nullptr)
+      return false;
+    return n->value != def;
+  } else {
+    if (n->sons[key[d]] != nullptr) {
+      return contains(n->sons[key[d]], key, d + 1);
+    }
+    return false;
+  }
 }
 
 template <class T, size_t R>
-bool Trie::trie<T,R>::erase(Trie::trie<T,R>::node_ptr & n,
-			     const std::string & key,
-			     size_t d,
-			     bool & decrease) {
-	if (key.size() == d && n != nullptr) {
-		if (n->value != def)
-			decrease = true;
-		n->value = T();
-		return n->s == 0;
-	} else {
-		if (n != nullptr && n->sons[key[d]] != nullptr) {
-			bool deleted = erase(n->sons[key[d]], key, d+1, decrease);
-			if (deleted) {
-				n->sons[key[d]].reset();
-				--n->s;
-				if (n->s == 0 && n->value == def)
-					return true;
-			}
-			return false;
-		}
-		return false;
-	}
-}
-
-
-template <class T, size_t R>
-std::vector<std::string> Trie::trie<T,R>::keys(const std::string & prefix) {
-	vec_ptr vec;
-	vec = vec_ptr(new std::vector<std::string>());
-	keys(root, prefix, 0, vec);
-	return *vec;
+void Trie::trie<T, R>::erase(const std::string &key) {
+  bool decrease = false;
+  erase(root, key, 0, decrease);
+  if (decrease)
+    --s;
 }
 
 template <class T, size_t R>
-void Trie::trie<T,R>::keys(Trie::trie<T,R>::node_ptr & n,
-			       std::string prefix,
-			       size_t d,
-			       vec_ptr & v) {
-	if (prefix.size() == d) {
-		gather_keys(n, prefix, v);
-	} else {
-		if (n->sons[prefix[d]] != nullptr)
-			keys(n->sons[prefix[d]], prefix, d+1, v);
-	}
+bool Trie::trie<T, R>::erase(Trie::trie<T, R>::node_ptr &n,
+                             const std::string &key, size_t d, bool &decrease) {
+  if (key.size() == d && n != nullptr) {
+    if (n->value != def)
+      decrease = true;
+    n->value = T();
+    return n->s == 0;
+  } else {
+    if (n != nullptr && n->sons[key[d]] != nullptr) {
+      bool deleted = erase(n->sons[key[d]], key, d + 1, decrease);
+      if (deleted) {
+        n->sons[key[d]].reset();
+        --n->s;
+        if (n->s == 0 && n->value == def)
+          return true;
+      }
+      return false;
+    }
+    return false;
+  }
 }
 
 template <class T, size_t R>
-void Trie::trie<T,R>::gather_keys(Trie::trie<T,R>::node_ptr & n,
-				  std::string prefix,
-				  vec_ptr & v) {
-	if (n->value != def) {
-		v->push_back(prefix);
-	}
-	if (n->s != 0) {
-		for (size_t i = 0; i < n->r; ++i) {
-			if (n->sons[i] != nullptr) {
-				gather_keys(n->sons[i], prefix + static_cast<char>(i), v);
-			}
-		}
-	}
+std::vector<std::string> Trie::trie<T, R>::keys(const std::string &prefix) {
+  vec_ptr vec;
+  vec = vec_ptr(new std::vector<std::string>());
+  keys(root, prefix, 0, vec);
+  return *vec;
 }
 
 template <class T, size_t R>
-void Trie::trie<T,R>::show() {
-        std::cout << "digraph graphName{" << std::endl;
-	std::cout << "node [shape=record];" << std::endl;
-	// Node labels
-	size_t label = 0;
-	std::cout << label << " [shape=record,label=\"{Root}\"];" << std::endl;;
-	for (size_t i = 0; i < root->r; ++i) {
-	        if (root->sons[i] != nullptr) {
-			++label;
-			show_label(root->sons[i], i, label);
-		}
-	}
-	// Node hierarchy
-	label = 0;
-	for (size_t i = 0; i < root->r; ++i) {
-	        if (root->sons[i] != nullptr) {
-                        std::cout << 0 << "->";
-			++label;
-			show(root->sons[i], i, label);
-		}
-	}
-	std::cout << "}"<< std::endl;
-}
-
-
-template <class T, size_t R>
-void Trie::trie<T,R>::show_label(Trie::trie<T,R>::node_ptr & n, size_t pos, size_t & label) {
-         std::cout << label << " [shape=record,label=\"{ <data>" << static_cast<char>(pos);
-	 if (n->value != T())
-	         std::cout << " | <value> " << n->value;
-	 std::cout << "}\"";
-	 if (n->value != T())
-	         std::cout << "color=\"blue\"";
-	 std:: cout << "];" << std::endl;
-         for (size_t i = 0; i < n->r; ++i) {
-	        if (n->sons[i] != nullptr) {
-		        ++label;
-			show_label(n->sons[i], i, label);
-		}
-	}
+void Trie::trie<T, R>::keys(Trie::trie<T, R>::node_ptr &n, std::string prefix,
+                            size_t d, vec_ptr &v) {
+  if (prefix.size() == d) {
+    gather_keys(n, prefix, v);
+  } else {
+    if (n->sons[prefix[d]] != nullptr)
+      keys(n->sons[prefix[d]], prefix, d + 1, v);
+  }
 }
 
 template <class T, size_t R>
-void Trie::trie<T,R>::show(Trie::trie<T,R>::node_ptr & n, size_t pos, size_t & label) {
-         std::cout << label << std::endl;
-	 size_t copy_label = label;
-         for (unsigned int i = 0; i < n->r; ++i) {
-	        if (n->sons[i] != nullptr) {
-		        std::cout << copy_label << "->";
-			++label;
-			show(n->sons[i], i, label);
-		}
-	}
+void Trie::trie<T, R>::gather_keys(Trie::trie<T, R>::node_ptr &n,
+                                   std::string prefix, vec_ptr &v) {
+  if (n->value != def) {
+    v->push_back(prefix);
+  }
+  if (n->s != 0) {
+    for (size_t i = 0; i < n->r; ++i) {
+      if (n->sons[i] != nullptr) {
+        gather_keys(n->sons[i], prefix + static_cast<char>(i), v);
+      }
+    }
+  }
+}
+
+template <class T, size_t R> void Trie::trie<T, R>::show() {
+  std::cout << "digraph graphName{" << std::endl;
+  std::cout << "node [shape=record];" << std::endl;
+  // Node labels
+  size_t label = 0;
+  std::cout << label << " [shape=record,label=\"{Root}\"];" << std::endl;
+  ;
+  for (size_t i = 0; i < root->r; ++i) {
+    if (root->sons[i] != nullptr) {
+      ++label;
+      show_label(root->sons[i], i, label);
+    }
+  }
+  // Node hierarchy
+  label = 0;
+  for (size_t i = 0; i < root->r; ++i) {
+    if (root->sons[i] != nullptr) {
+      std::cout << 0 << "->";
+      ++label;
+      show(root->sons[i], i, label);
+    }
+  }
+  std::cout << "}" << std::endl;
 }
 
 template <class T, size_t R>
-std::string Trie::trie<T,R>::lcp() {
-	return lcp(root, "");
+void Trie::trie<T, R>::show_label(Trie::trie<T, R>::node_ptr &n, size_t pos,
+                                  size_t &label) {
+  std::cout << label << " [shape=record,label=\"{ <data>"
+            << static_cast<char>(pos);
+  if (n->value != T())
+    std::cout << " | <value> " << n->value;
+  std::cout << "}\"";
+  if (n->value != T())
+    std::cout << "color=\"blue\"";
+  std::cout << "];" << std::endl;
+  for (size_t i = 0; i < n->r; ++i) {
+    if (n->sons[i] != nullptr) {
+      ++label;
+      show_label(n->sons[i], i, label);
+    }
+  }
 }
 
 template <class T, size_t R>
-std::string Trie::trie<T,R>::lcp(Trie::trie<T,R>::node_ptr &n, std::string s) {
-	if (n->s == 1) {
-		for (unsigned int i = 0; i < n->r; ++i) {
-			if (n->sons[i] != nullptr) {
-				return lcp(n->sons[i], s + static_cast<char>(i));
-			}
-		}
-	}
-	return s;
+void Trie::trie<T, R>::show(Trie::trie<T, R>::node_ptr &n, size_t pos,
+                            size_t &label) {
+  std::cout << label << std::endl;
+  size_t copy_label = label;
+  for (unsigned int i = 0; i < n->r; ++i) {
+    if (n->sons[i] != nullptr) {
+      std::cout << copy_label << "->";
+      ++label;
+      show(n->sons[i], i, label);
+    }
+  }
+}
+
+template <class T, size_t R> std::string Trie::trie<T, R>::lcp() {
+  return lcp(root, "");
+}
+
+template <class T, size_t R>
+std::string Trie::trie<T, R>::lcp(Trie::trie<T, R>::node_ptr &n,
+                                  std::string s) {
+  if (n->s == 1) {
+    for (unsigned int i = 0; i < n->r; ++i) {
+      if (n->sons[i] != nullptr) {
+        return lcp(n->sons[i], s + static_cast<char>(i));
+      }
+    }
+  }
+  return s;
 };
--- trie/lib/trie.h
+++ trie/lib/trie.h
@@ -8,211 +8,190 @@
 #include <memory>
 
 namespace Trie {
-	/*
-	 * Trie
-	 */
-	template <class T, size_t R>
-		class trie {
-	public:
-		// Constructor
-		explicit trie() : r(R), s(0) {
-			root = node_ptr(new node);
-			aux_ret = T();
-		}
-		// Destructor
-		~trie() {
-			clear(std::move(root));
-		}
-		// Methods
-		/**
-		 * This method is to allow the user to clear the
-		 * content of the trie. Basically does the same
-		 * as the destructor.
-		 */
-		void clear() {
-		  clear(std::move(root));
-		  s = 0;
-		  root = node_ptr(new node);
-		}
+/*
+ * Trie
+ */
+template <class T, size_t R> class trie {
+public:
+  // Constructor
+  explicit trie() : r(R), s(0) {
+    root = node_ptr(new node);
+    aux_ret = T();
+  }
+  // Destructor
+  ~trie() { clear(std::move(root)); }
+  // Methods
+  /**
+   * This method is to allow the user to clear the
+   * content of the trie. Basically does the same
+   * as the destructor.
+   */
+  void clear() {
+    clear(std::move(root));
+    s = 0;
+    root = node_ptr(new node);
+  }
 
-		/**
-		 * This method return the value stored in the trie
-		 * for a specific key provided as parameter. If there
-		 * is no the key in the trie return default value for
-		 * the value type.
-		 */
-		const T & find(const std::string & key);
+  /**
+   * This method return the value stored in the trie
+   * for a specific key provided as parameter. If there
+   * is no the key in the trie return default value for
+   * the value type.
+   */
+  const T &find(const std::string &key);
 
-		/**
-		 * This method is used to add a new key to the trie.
-		 * You must provide the key and the value to store.
-		 * If the key was not before in the trie it will
-		 * create the path to it and store the value.
-		 * Otherwise, if the key was already in the trie it
-		 * will update the value of the key with the new one.
-		 */
-		void insert(const std::string & key,
-			 const T & value);
+  /**
+   * This method is used to add a new key to the trie.
+   * You must provide the key and the value to store.
+   * If the key was not before in the trie it will
+   * create the path to it and store the value.
+   * Otherwise, if the key was already in the trie it
+   * will update the value of the key with the new one.
+   */
+  void insert(const std::string &key, const T &value);
 
-		/**
-		 * This method return the amount of elements stored
-		 * in the trie.
-		 */
-		size_t size() {return s;}
+  /**
+   * This method return the amount of elements stored
+   * in the trie.
+   */
+  size_t size() { return s; }
 
-		/**
-		 * This is an auxiliary method to print in the screen
-		 * the current set of keys of the trie.
-		 */
-		void show();
+  /**
+   * This is an auxiliary method to print in the screen
+   * the current set of keys of the trie.
+   */
+  void show();
 
-		/**
-		 * This method is to remove a value from the trie.
-		 * You must provide the key that holds the value.
-		 * If the key exist deletes the value and delete
-		 * the path to it if there are no other keys that
-		 * are using it. Otherwise, if the key is not there
-		 * nothing will happen.
-		 */
-		void erase(const std::string & key);
+  /**
+   * This method is to remove a value from the trie.
+   * You must provide the key that holds the value.
+   * If the key exist deletes the value and delete
+   * the path to it if there are no other keys that
+   * are using it. Otherwise, if the key is not there
+   * nothing will happen.
+   */
+  void erase(const std::string &key);
 
-		/**
-		 * This method tells you if a given key is in the
-		 * trie.
-		 */
-		bool contains(const std::string & key);
+  /**
+   * This method tells you if a given key is in the
+   * trie.
+   */
+  bool contains(const std::string &key);
 
-		/**
-		 * This method returns a vector of all keys in the
-		 * trie with the prefix provided. If there is no
-		 * prefix provided it will return all the keys in
-		 * the trie.
-		 */
-		std::vector<std::string> keys(const std::string & prefix = "");
+  /**
+   * This method returns a vector of all keys in the
+   * trie with the prefix provided. If there is no
+   * prefix provided it will return all the keys in
+   * the trie.
+   */
+  std::vector<std::string> keys(const std::string &prefix = "");
 
-		/**
-		 * This method returns the longest common prefix
-		 * of the content in the Trie.
-		 */
-		std::string lcp();
+  /**
+   * This method returns the longest common prefix
+   * of the content in the Trie.
+   */
+  std::string lcp();
 
-	private:
-		/*
-		 * Node
-		 */
-		struct node {
-			T value;
-			std::array<std::unique_ptr<node>, R> sons;
-			size_t s;
-			size_t r;
+private:
+  /*
+   * Node
+   */
+  struct node {
+    T value;
+    std::array<std::unique_ptr<node>, R> sons;
+    size_t s;
+    size_t r;
 
-			explicit node(T v = T()) : value(v), s(0), r(R) {
-				sons = std::array<std::unique_ptr<node>, R>();
-				for (size_t i = 0; i < r; ++i) {
-					sons[i] = nullptr;
-				}
-			}
-		};
+    explicit node(T v = T()) : value(v), s(0), r(R) {
+      sons = std::array<std::unique_ptr<node>, R>();
+      for (size_t i = 0; i < r; ++i) {
+        sons[i] = nullptr;
+      }
+    }
+  };
 
-		/*
-		 * Utils
-		 */
-		using node_ptr = std::unique_ptr<node>;
-		using vec_ptr = std::unique_ptr<std::vector<std::string>>;
+  /*
+   * Utils
+   */
+  using node_ptr = std::unique_ptr<node>;
+  using vec_ptr = std::unique_ptr<std::vector<std::string>>;
 
+  // Attributes
+  node_ptr root;
+  size_t r;
+  size_t s;
+  // Default value of T to return.
+  const T def = T();
+  // Helper value to help return found values.
+  T aux_ret;
+  // Methods
+  /*
+   * Given a node it checks if the d-th position in
+   * the key exists in the node's sons. If that is
+   * true call himself but with d+1. When the key
+   * length is equal to d we check if the value
+   * is diferent from the default value for T.
+   */
+  node_ptr find(node_ptr &n, const std::string &key, size_t d);
 
-		// Attributes
-		node_ptr root;
-		size_t r;
-		size_t s;
-		// Default value of T to return.
-		const T def = T();
-		// Helper value to help return found values.
-		T aux_ret;
-		// Methods
-		/*
-		 * Given a node it checks if the d-th position in
-		 * the key exists in the node's sons. If that is
-		 * true call himself but with d+1. When the key
-		 * length is equal to d we check if the value
-		 * is diferent from the default value for T.
-		 */
-		node_ptr find(node_ptr & n,
-				  const std::string & key,
-				  size_t d);
+  /*
+   * Given a node it checks if the d-th position in
+   * the key exists, if not exists it is created.
+   * When the key length is equal to d we assign
+   * the value to the node's value.
+   */
+  node_ptr insert(node_ptr n, const std::string &key, const T &value, size_t d,
+                  bool &created);
 
-		/*
-		 * Given a node it checks if the d-th position in
-		 * the key exists, if not exists it is created.
-		 * When the key length is equal to d we assign
-		 * the value to the node's value.
-		 */
-		node_ptr insert(node_ptr n,
-				  const std::string & key,
-				  const T & value,
-				  size_t d,
-				  bool & created);
+  /*
+   * Given a node deleted everything under it.
+   */
+  void clear(node_ptr n);
 
-		/*
-		 * Given a node deleted everything under it.
-		 */
-		void clear(node_ptr n);
+  /*
+   * Given a node it checks if the d-th position in
+   * the key exists. When the key length is equal to
+   * d we begin to delete all the nodes from that
+   * one up to the first node with at least one son.
+   */
+  bool erase(node_ptr &n, const std::string &key, size_t d, bool &decrease);
 
-		/*
-		 * Given a node it checks if the d-th position in
-		 * the key exists. When the key length is equal to
-		 * d we begin to delete all the nodes from that
-		 * one up to the first node with at least one son.
-		 */
-		bool erase(node_ptr & n,
-			    const std::string & key,
-			    size_t d,
-			    bool & decrease);
+  /*
+   * Given a node it checks if the d-th position in
+   * the key exists. When the key length is equal to
+   * d we check if the value is different from the
+   * default value of T. If all that happens return
+   * true else return false.
+   */
+  bool contains(node_ptr &n, const std::string &key, size_t d);
 
-		/*
-		 * Given a node it checks if the d-th position in
-		 * the key exists. When the key length is equal to
-		 * d we check if the value is different from the
-		 * default value of T. If all that happens return
-		 * true else return false.
-		 */
-		bool contains(node_ptr & n,
-			      const std::string & key,
-			      size_t d);
+  /*
+   * Given a node it checks if the d-th position in
+   * the prefix exists. When the prefix length is
+   * equal to d then it calls gather keys to get
+   * all keys with that prefix.
+   */
+  void keys(node_ptr &n, std::string prefix, size_t d, vec_ptr &v);
+  /*
+   * Given a node goes to all its sons to gather
+   * all the keys that are under the given node.
+   * All those keys are stored in the vector.
+   */
+  void gather_keys(node_ptr &n, std::string prefix, vec_ptr &v);
 
-		/*
-		 * Given a node it checks if the d-th position in
-		 * the prefix exists. When the prefix length is
-		 * equal to d then it calls gather keys to get
-		 * all keys with that prefix.
-		 */
-		void keys(node_ptr & n,
-			      std::string prefix,
-			      size_t d,
-			      vec_ptr & v);
-		/*
-		 * Given a node goes to all its sons to gather
-		 * all the keys that are under the given node.
-		 * All those keys are stored in the vector.
-		 */
-		void gather_keys(node_ptr & n,
-				 std::string prefix,
-				 vec_ptr & v);
+  /*
+   * Given a node prints the its value and the
+   * ones for its sons in the 'dot' format.
+   */
+  void show_label(node_ptr &n, size_t pos, size_t &label);
+  void show(node_ptr &n, size_t pos, size_t &label);
 
-
-		/*
-		 * Given a node prints the its value and the
-		 * ones for its sons in the 'dot' format.
-		 */
-		void show_label(node_ptr & n, size_t pos, size_t & label);
-		void show(node_ptr & n, size_t pos, size_t & label);
-
-		/*
-		 * Returns the longest common prefix
-		 */
-		std::string lcp(node_ptr &n, std::string s);
-	}; // TRIE_CLASS
+  /*
+   * Returns the longest common prefix
+   */
+  std::string lcp(node_ptr &n, std::string s);
+}; // TRIE_CLASS
 
 }; // TRIE_NAMESPACE
 
-#endif  // TRIE_TRIE_H_
+#endif // TRIE_TRIE_H_
