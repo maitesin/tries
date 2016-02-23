2af0ade3f73df9735b4a06fc661d0223d568162a
Maitesin <violador.de.segmentos@gmail.com>
2016-02-23 21:31:44 +0000

    Added dictionary versions to run the code.

--- test/scripts/common.sh
+++ test/scripts/common.sh
@@ -3,8 +3,25 @@ exec_test() {
     ./${exec} | tee ../data/${exec}.output
 }
 
+exec_test_dict() {
+    local exec=$1; shift
+    local dict=$1; shift
+    local name=$(echo ${dict} | rev | cut -d="/" -f1 | rev)
+    ./${exec} ${dict} | tee ../data/${exec}_${name}.output
+}
+
 traverse() {
     local suffix=$1; shift
+    for i in $(ls *${suffix})
+    do
+	echo $(echo ${i} | sed "s/${suffix}//g")
+	exec_test ${i}
+    done
+}
+
+traverse_dict() {
+    local suffix=$1; shift
+    local dict=$1; shift
     for i in $(ls *${suffix})
     do
 	echo $(echo ${i} | sed "s/${suffix}//g")
---
+++ test/scripts/remove_found_dictionary.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Import common functions
+source common.sh
+# Move to the parent folder
+cd ../bin
+# Traverse dictionaries
+for dict in $(ls ../dict/*.txt)
+do
+        traverse_dict _remove_found ../dict/${dict}
+done
