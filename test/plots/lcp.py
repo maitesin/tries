e68ef17cefb05705b2376569a2c8841f76ebf2bd
Oscar Forner Martinez <oscar_martinez@programmingresearch.com>
2016-02-24 08:50:07 +0000

    Commit stuff to run the Longest Common Path test and generate the graphics.

---
+++ test/plots/lcp.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python2.7
+
+from numpy import *
+from pylab import *
+from matplotlib import rc, rcParams
+
+rc('font',**{'family':'serif','serif':['Computer Modern']})
+
+trie = genfromtxt('../data/trie_lcp.output')
+tst = genfromtxt('../data/tst_lcp.output')
+radix = genfromtxt('../data/radix_lcp.output')
+_map = genfromtxt('../data/map_lcp.output')
+umap  = genfromtxt('../data/umap_lcp.output')
+
+######## TIME ########
+plot(trie[:,0], trie[:,1], '-o', label='Trie')
+hold(True)
+plot(tst[:,0], tst[:,1], '-o', label='Ternary Search Tree')
+plot(radix[:,0], radix[:,1], '-o', label='Radix Tree')
+plot(_map[:,0], _map[:,1], '-o', label='STL ordered Map')
+plot(umap[:,0], umap[:,1], '-o', label='STL unordered Map')
+
+xlabel('Max length of the string')
+ylabel('Time(ms)')
+title('Remove test (found)')
+legend(loc='best')
+grid(True)
+savefig('lcp_time_ALL.eps')
+
+hold(False)
+plot(tst[:,0], tst[:,1], '-o', label='Ternary Search Tree')
+hold(True)
+plot(radix[:,0], radix[:,1], '-o', label='Radix Tree')
+plot(_map[:,0], _map[:,1], '-o', label='STL ordered Map')
+plot(umap[:,0], umap[:,1], '-o', label='STL unordered Map')
+
+xlabel('Max length of the string')
+ylabel('Time(ms)')
+title('Remove test (found)')
+legend(loc='best')
+grid(True)
+savefig('lcp_time_TRMU.eps')
+
+hold(False)
+plot(radix[:,0], radix[:,1], '-o', label='Radix Tree')
+hold(True)
+plot(_map[:,0], _map[:,1], '-o', label='STL ordered Map')
+plot(umap[:,0], umap[:,1], '-o', label='STL unordered Map')
+
+xlabel('Max length of the string')
+ylabel('Time(ms)')
+title('Remove test (found)')
+legend(loc='best')
+grid(True)
+savefig('lcp_time_RMU.eps')
